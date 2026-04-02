0dc4c635f86edc10bd02402ff6b03b3e40a7b86c
akeliansevag <akelian.sevag@gmail.com>
2026-04-02 10:48:51 +0300

    new article comium

---
+++ comium-completes-ramadan-csr-initiative-donates-d1-million-to-each-beneficiary.php
@@ -0,0 +1,271 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Google tag (gtag.js) -->
+    <base href="/" />
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-EBLZZJYD58"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-EBLZZJYD58');
+    </script>
+    <meta charset="utf-8" />
+    <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+    <title>Comium - International Roaming</title>
+    <meta content="" name="description" />
+    <meta content="" name="keywords" />
+    <meta name="robots" content=”noindex”>
+    <!-- Favicons -->
+    <link href="assets/img/logo-rd.svg" rel="icon" />
+
+    <!-- Vendor CSS Files -->
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet" />
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+    <!--<link rel="stylesheet" href="https://use.typekit.net/yfw8tzl.css" />-->
+    <!--<link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet" />-->
+    <link href="assets/vendor/bootstrap-icons-new/font/bootstrap-icons.css" rel="stylesheet" />
+    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet" />
+    <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet" />
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet" />
+    <!-- Template Main CSS File -->
+    <link href="assets/css/style.css?v36" rel="stylesheet" />
+    <link href="assets/css/main.css?v57" rel="stylesheet" />
+    <link href="assets/css/data-bundles.css?v=3" rel="stylesheet" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+    <link href="assets/css/slick.min.css" rel="stylesheet" />
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.5.9/slick-theme.min.css" />
+
+    <link href="assets/international-roaming/select2.min.css" rel="stylesheet" />
+    <link rel="stylesheet" type="text/css" href="assets/international-roaming/style.css?v=1" />
+
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="COMIUM COMPLETES RAMADAN CSR INITIATIVE, DONATES D1 MILLION TO EACH BENEFICIARY" />
+    <meta property="og:description" content="Banjul, The Gambia - Comium has successfully concluded its Ramadan corporate social responsibility initiative, donating Dl,000,000 to each of three institutions, reinforcing its commitment to supporting communities across the country." />
+    <meta property="og:image" content="https://comium.gm/images/og-default.jpg" />
+    <meta property="og:url" content="https://comium.gm/comium-completes-ramadan-csr-initiative-donates-d1-million-to-each-beneficiary" />
+    <meta property="og:site_name" content="Comium" />
+
+    <!-- Optional but recommended -->
+    <meta property="og:locale" content="en_US" />
+
+    <!-- Twitter (X) fallback -->
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:title" content="COMIUM COMPLETES RAMADAN CSR INITIATIVE, DONATES D1 MILLION TO EACH BENEFICIARY" />
+    <meta name="twitter:description" content="Banjul, The Gambia - Comium has successfully concluded its Ramadan corporate social responsibility initiative, donating Dl,000,000 to each of three institutions, reinforcing its commitment to supporting communities across the country." />
+    <meta name="twitter:image" content="https://comium.gm/comium-completes-ramadan-csr-initiative-donates-d1-million-to-each-beneficiary/images/og-default.jpg" />
+</head>
+
+<body>
+    <!-- ======= Header ======= -->
+    <header id="header" class="header fixed-top">
+        <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
+            <a href="/" class="logo d-flex align-items-center">
+                <img src="assets/img/logo-rd.svg" alt="comium" style="    height: 74px;padding: 20px 0px;" />
+                <!-- <span>FlexStart</span> -->
+            </a>
+            <nav id="navbar" class="navbar">
+                <ul>
+                    <li>
+                        <a class="nav-link scrollto hover-careers" href="/#about">About <svg class="careers-svg" width="19" height="19" viewBox="0 0 19 19" stroke="black" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M7.125 14.25L11.875 9.5L7.125 4.75" fill="transparent" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round" />
+                            </svg>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="nav-link scrollto hover-careers" href="/#products">Products & Services <svg class="careers-svg" width="19" height="19" viewBox="0 0 19 19" stroke="black" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M7.125 14.25L11.875 9.5L7.125 4.75" fill="transparent" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round" />
+                            </svg>
+                        </a>
+                    </li>
+                    <li class="nav-item dropdown ip-dropdown">
+                        <a
+                            class="nav-link ip-trigger"
+                            href="#"
+                            id="internationalProductsDropdown"
+                            role="button"
+                            data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            <span>International Products</span>
+
+                            <!-- Chevron -->
+                            <svg class="ip-chevron" style="visibility: visible;" width="16" height="16" viewBox="0 0 24 24" fill="none"
+                                xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                                <path d="M6 9l6 6 6-6"
+                                    stroke="currentColor"
+                                    stroke-width="2.5"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round" />
+                            </svg>
+                        </a>
+
+                        <ul class="dropdown-menu ip-menu" aria-labelledby="internationalProductsDropdown">
+                            <li><a class="dropdown-item" style="margin:0;background:none;" href="/international-direct-dialing">International Direct Dialing</a></li>
+                            <li><a class="dropdown-item" style="margin:0;background:none;" href="/international-roaming">International Roaming</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a class="nav-link scrollto hover-careers" href="/#careers">Careers <svg class="careers-svg" width="19" height="19" viewBox="0 0 19 19" stroke="black" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M7.125 14.25L11.875 9.5L7.125 4.75" fill="transparent" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round" />
+                            </svg>
+                        </a>
+                    </li>
+                    <li>
+                    <li>
+                        <a class="nav-link scrollto hover-careers" href="/#contact">Contact <svg class="careers-svg" width="19" height="19" viewBox="0 0 19 19" stroke="black" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M7.125 14.25L11.875 9.5L7.125 4.75" fill="transparent" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round" />
+                            </svg>
+                        </a>
+                    </li>
+                </ul>
+                <i class="bi bi-list mobile-nav-toggle"></i>
+            </nav>
+            <!-- .navbar -->
+        </div>
+    </header>
+
+    <section class="my-5">
+        <div class="container">
+            <h1 class="font-bold fs-1">COMIUM COMPLETES RAMADAN CSR INITIATIVE, DONATES D1 MILLION TO EACH BENEFICIARY</h1>
+            <p>
+                Banjul, The Gambia - Comium has successfully concluded its Ramadan corporate social responsibility initiative, donating Dl,000,000 to each of three institutions, reinforcing its commitment to supporting communities across the country.
+                The beneficiaries include SOS Children's Villages, the Shelter for Children and the Elderly, and the Gambia Supreme lslamic Council. This contribution is part of Comium's "D1 for Good" campaign, which connected everyday customer activity to meaningful community impact throughout the holy month.
+                The initiative was designed to ensure that every transaction made during Ramadan contributed to a greater purpose - transforming daily connectivity into acts of giving.
+            </p>
+            <div class="mb-3">
+                <img class="w-100" src="/assets/images/comium-article.webp" />
+            </div>
+            <p class="py-3 border border-start-0 border-end-0 fs-5">
+                "Ramadan reminds us of the importance of compassion, unity, and giving back. Through this initiative, we wanted every connection to carry meaning and contribute to our communities," said CEO Jad Nassif.
+            </p>
+            <p>
+                The donation handovers marked the culmination of the campaign, with beneficiaries expressing gratitude and highlighting the positive impact this support will bring - from assisting vulnerable children to supporting the elderly and strengthening community welfare.
+            </p>
+            <p>
+                As the campaign concludes, it stands as a reminder that collective efforts can create lasting impact, and that even the simplest actions can make a meaningful difference.
+            </p>
+        </div>
+        <div class="container mt-5 d-flex flex-column flex-lg-row gap-5 align-items-center">
+            <div class="">
+                <img class="w-100" style="max-width: 200px;" src="/assets/images/the-gambia-supreme-islamic-council.webp" alt="">
+            </div>
+            <div class="">
+                <img class="w-100" style="max-width: 200px;" src="/assets/images/mogcsw-logo.webp" alt="">
+            </div>
+            <div class="">
+                <img class="w-100" style="max-width: 500px;" src="/assets/images/sos-childrens-villages.webp" alt="">
+            </div>
+            <div>
+                <img class="w-100" src="assets/img/logo-rd.svg" alt="comium">
+            </div>
+        </div>
+    </section>
+
+    <footer id="footer" class="footer">
+
+        <div class="footer-top" style="background:#f9f9f9">
+            <div class="container">
+                <div class="row gy-4">
+                    <div class="col-lg-5 col-md-12 footer-info">
+                        <a href="index.html" class="logo d-flex align-items-center">
+                            <img src="assets/img/logo-rd.svg" alt="footer" style="    height: 37px; " />
+                        </a>
+                        <p> At Comium work in favor of making communications ever convenient for our subscribers </p>
+                        <div class="social-links mt-3">
+                            <a href="https://twitter.com/ComiumGm" target="_blank" class="twitter">
+                                <i class="bi bi-twitter-x"></i>
+                            </a>
+                            <a href="https://www.facebook.com/Comium.Gambia" target="_blank" class="facebook">
+                                <i class="bi bi-facebook"></i>
+                            </a>
+                            <a href="https://www.instagram.com/comium.gm/" target="_blank" class="instagram">
+                                <i class="bi bi-instagram"></i>
+                            </a>
+                            <a href="https://www.linkedin.com/company/comium-gambia/" target="_blank" class="linkedin">
+                                <i class="bi bi-linkedin"></i>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-12 footer-contact  text-md-start useful-link">
+                        <h4>Useful Links</h4>
+                        <ul class="no-bullets">
+                            <li>
+                                <i class="bi bi-chevron-right"></i>
+                                <a href="#products">Products & Services</a>
+                            </li>
+                            <li>
+                                <i class="bi bi-chevron-right"></i>
+                                <a href="#about">About</a>
+                            </li>
+                            <li>
+                                <i class="bi bi-chevron-right"></i>
+                                <a href="#careers">Careers</a>
+                            </li>
+                            <li>
+                                <i class="bi bi-chevron-right"></i>
+                                <a href="#contact">Contact</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-3 col-md-12 footer-contact  text-md-start">
+                        <h4>Contact Us</h4>
+                        <p> 27 Kairaba Avenue <br /> Pipeline,KSMD <br /> Banjul-The Gambia <br />
+                            <strong>Monday</strong> to <strong>Friday</strong> 8:30 AM - 5:00 PM</strong> <br />
+                            <strong>Saturday:</strong> 9:00 AM - 2:00 PM <br />
+                            <strong>Phone:</strong>
+                            <a href="tel:+220 6 601 111" style="color:black" onMouseOver="this.style.color='red'" onMouseOut="this.style.color='black'">+220 6 601 111</a>
+                            <br />
+                            <strong>Email:</strong>
+                            <a href="mailto:info@comium.gm " style="color:black" onMouseOver="this.style.color='red'" onMouseOut="this.style.color='black'">info@comium.gm</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="copyright"> &copy; Copyright <strong>
+                    <span>Comium</span>
+                </strong>. All Rights Reserved </div>
+            <div class="credits"></div>
+        </div>
+    </footer>
+    <!-- End Footer -->
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center">
+        <i class="bi bi-arrow-up-short"></i>
+    </a>
+
+
+    <!-- Vendor JS Files -->
+
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/main.js?v9"></script>
+    <script src="assets/js/jquery-3.5.0.min.js"></script>
+    <script src="assets/js/slick.min.js"></script>
+
+    <script src="assets/js/mixall.js?v10"></script>
+
+    <script src="assets/vendor/aos/aos.js"></script>
+    <script src="assets/international-roaming/select2.min.js"></script>
+    <script src="assets/international-roaming/script.js?v=1"></script>
+
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-XJHHRG8NCG"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+        gtag('config', 'G-XJHHRG8NCG');
+    </script>
+</body>
+
+</html>

dd88d49ff8ce42a96d1e776177a9872d0ad82e3e
Sevag Akelian <akelian.sevag@gmail.com>
2026-07-13 15:07:24 +0300

    .

---
+++ international-roaming copy.php
@@ -0,0 +1,370 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Google tag (gtag.js) -->
+    <base href="/" />
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-EBLZZJYD58"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-EBLZZJYD58');
+    </script>
+    <meta charset="utf-8" />
+    <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+    <title>Comium - International Roaming</title>
+    <meta content="" name="description" />
+    <meta content="" name="keywords" />
+    <meta name="robots" content=”noindex”>
+    <!-- Favicons -->
+    <link href="assets/img/logo-rd.svg" rel="icon" />
+
+    <!-- Vendor CSS Files -->
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet" />
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+    <!--<link rel="stylesheet" href="https://use.typekit.net/yfw8tzl.css" />-->
+    <!--<link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet" />-->
+    <link href="assets/vendor/bootstrap-icons-new/font/bootstrap-icons.css" rel="stylesheet" />
+    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet" />
+    <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet" />
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet" />
+    <!-- Template Main CSS File -->
+    <link href="assets/css/style.css?v36" rel="stylesheet" />
+    <link href="assets/css/main.css?v57" rel="stylesheet" />
+    <link href="assets/css/data-bundles.css?v=3" rel="stylesheet" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+    <link href="assets/css/slick.min.css" rel="stylesheet" />
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.5.9/slick-theme.min.css" />
+
+    <link href="assets/international-roaming/select2.min.css" rel="stylesheet" />
+    <link rel="stylesheet" type="text/css" href="assets/international-roaming/style.css?v=1" />
+</head>
+
+<body>
+    <!-- ======= Header ======= -->
+    <header id="header" class="header fixed-top">
+        <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
+            <a href="/" class="logo d-flex align-items-center">
+                <img src="assets/img/logo-rd.svg" alt="comium" style="    height: 74px;padding: 20px 0px;" />
+                <!-- <span>FlexStart</span> -->
+            </a>
+            <nav id="navbar" class="navbar">
+                <ul>
+                    <li>
+                        <a class="nav-link scrollto hover-careers" href="/#about">About
+                        </a>
+                    </li>
+                    <li>
+                        <a class="nav-link scrollto hover-careers" href="/#products">Products & Services
+                        </a>
+                    </li>
+                    <li class="nav-item dropdown ip-dropdown">
+                        <a
+                            class="nav-link ip-trigger"
+                            href="#"
+                            id="internationalProductsDropdown"
+                            role="button"
+                            data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            <span>International Products</span>
+
+                            <!-- Chevron -->
+                            <svg class="ip-chevron" style="visibility: visible;" width="16" height="16" viewBox="0 0 24 24" fill="none"
+                                xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                                <path d="M6 9l6 6 6-6"
+                                    stroke="currentColor"
+                                    stroke-width="2.5"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round" />
+                            </svg>
+                        </a>
+
+                        <ul class="dropdown-menu ip-menu" aria-labelledby="internationalProductsDropdown">
+                            <li><a class="dropdown-item" style="margin:0;background:none;" href="/international-direct-dialing">International Direct Dialing</a></li>
+                            <li><a class="dropdown-item" style="margin:0;background:none;" href="/international-roaming">International Roaming</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a class="nav-link scrollto hover-careers" href="/#careers">Careers
+                        </a>
+                    </li>
+                    <li>
+                    <li>
+                        <a class="nav-link scrollto hover-careers" href="/#contact">Contact
+                        </a>
+                    </li>
+                    <li>
+                        <a class="nav-link standout" href="/mycomium-app">MyComium Self-care APP
+                        </a>
+                    </li>
+                </ul>
+                <i class="bi bi-list mobile-nav-toggle"></i>
+            </nav>
+            <!-- .navbar -->
+        </div>
+    </header>
+
+    <div class="roaming-banner">
+        <img src='/assets/international-roaming/banner.webp' />
+        <div class="container">
+            <div class="half-width">
+                <h1>ROAM FREELY, <br /> CONNECT GLOBALLY.</h1>
+                <p>Stay connected worldwide with seamless calls, texts, and data
+                    wherever you go.</p>
+            </div>
+
+        </div>
+    </div>
+    <div class="bc">
+        <div class="container">
+            <div class="bc-content">
+                <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M1 6.04337C1 5.66046 1 5.46901 1.04935 5.2927C1.09307 5.13652 1.16491 4.98963 1.26135 4.85924C1.37022 4.71204 1.52135 4.5945 1.82359 4.35942L6.34513 0.842666C6.57935 0.660497 6.69646 0.569413 6.82577 0.5344C6.93987 0.503507 7.06013 0.503507 7.17423 0.5344C7.30354 0.569413 7.42065 0.660497 7.65487 0.842666L12.1764 4.35942C12.4787 4.5945 12.6298 4.71204 12.7386 4.85924C12.8351 4.98963 12.9069 5.13652 12.9506 5.2927C13 5.46901 13 5.66046 13 6.04337V10.8667C13 11.6134 13 11.9868 12.8547 12.272C12.7268 12.5229 12.5229 12.7268 12.272 12.8547C11.9868 13 11.6134 13 10.8667 13H3.13333C2.3866 13 2.01323 13 1.72801 12.8547C1.47713 12.7268 1.27316 12.5229 1.14532 12.272C1 11.9868 1 11.6134 1 10.8667V6.04337Z" stroke="#B50000" stroke-linecap="round" stroke-linejoin="round" />
+                </svg>
+                <p>
+                    Home > International Roaming
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <?php
+    $filter = $_GET['filter'] ?? 'overview';
+    $filters = [
+        'overview' => 'Overview',
+        'roaming-tariff' => 'Roaming Tariff',
+        'roaming-bundles' => 'Roaming Bundles',
+        'roaming-complaints' => 'Roaming Complaints',
+        'faqs-and-troubleshooting' => 'FAQs & Troubleshooting'
+    ];
+
+    ?>
+
+
+    <section>
+        <div class="container">
+            <div>
+                <ul class="nav international-roaming-filters">
+                    <?php foreach ($filters as $key => $f): ?>
+                        <li class="<?= $filter == $key ? 'active' : '' ?>"
+                            data-bs-toggle="pill"
+                            data-bs-target="#<?= $key ?>"
+                            data-filter="<?= $key ?>"
+                            role="tab">
+                            <?= $f ?>
+                        </li>
+                    <?php endforeach; ?>
+                </ul>
+                <div class="tab-content international-roaming-content">
+                    <div class="tab-pane fade <?= $filter == 'overview' ? 'show active' : '' ?>" id="overview">
+                        <h3 class="red-title">Overview</h3>
+
+                        <h5>
+                            Stay effortlessly connected, no matter where your travels take you with COMIUM Gambia’s international roaming SIM Card.
+                        </h5>
+
+                        <p>
+                            Experience smooth, high-quality communication, seamless data, and uninterrupted service across partner networks worldwide – all while keeping your COMIUM number.
+                        </p>
+
+                        <p>
+                            <i>Travel with confidence, knowing that you’re always just a call away with <span class="red-text"><strong>Comium!</strong></span></i>
+                        </p>
+
+                        <hr class="my-4">
+
+                        <h4 class="red-title">Introducing the New USSD Menu for Roaming Bundles</h4>
+
+                        <p>
+                            Buy your Comium roaming bundles directly via USSD by dialing
+                            <strong class="red-text">*622#</strong>.
+                        </p>
+
+                        <p>
+                            Easy, direct, and more accessible.
+                        </p>
+
+                        <h5 class="mt-4">
+                            Enjoy strong and reliable internet worldwide
+                        </h5>
+
+                        <div class="table-responsive mt-3">
+                            <table class="table table-bordered table-striped align-middle">
+                                <thead>
+                                    <tr>
+                                        <th>Bundle</th>
+                                        <th>Validity</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>100MB</td>
+                                        <td>3 Days</td>
+                                    </tr>
+                                    <tr>
+                                        <td>200MB</td>
+                                        <td>3 Days</td>
+                                    </tr>
+                                    <tr>
+                                        <td>500MB</td>
+                                        <td>7 Days</td>
+                                    </tr>
+                                    <tr>
+                                        <td>1GB</td>
+                                        <td>15 Days</td>
+                                    </tr>
+                                    <tr>
+                                        <td>3GB</td>
+                                        <td>20 Days</td>
+                                    </tr>
+                                    <tr>
+                                        <td>5GB</td>
+                                        <td>30 Days</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+
+                    <div class="tab-pane fade <?= $filter == 'roaming-tariff' ? 'show active' : '' ?> " id="roaming-tariff">
+                        <?php include_once('components/roaming-tariff.php'); ?>
+                    </div>
+
+
+                    <div class="tab-pane fade <?= $filter == 'roaming-bundles' ? 'show active' : '' ?> " id="roaming-bundles">
+                        <?php include_once('components/roaming-bundles.php'); ?>
+                    </div>
+                    <div class="tab-pane fade <?= $filter == 'roaming-complaints' ? 'show active' : '' ?> " id="roaming-complaints">
+                        <?php include_once('components/complaints.php'); ?>
+                    </div>
+                    <div class="tab-pane fade <?= $filter == 'faqs-and-troubleshooting' ? 'show active' : '' ?> " id="faqs-and-troubleshooting">
+                        <?php include_once('components/faqs.php'); ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="customer-service">
+        <div class="container text-center">
+            <h6>Customer Service</h6>
+            <p>We, at Comium offer the best-in-class customer service. Our team of trained professionals is dedicated to you
+                and always ready to assist you, answer all your enquiries, and explain any service or offer you need 24/7.</p>
+            <p>
+                Call Comium centers on <span class="red-text">111</span> for free Or send us an email at <a href="mailto:info@comium.gm">info@Comium.gm</a>
+            </p>
+        </div>
+    </section>
+
+    <footer id="footer" class="footer">
+
+        <div class="footer-top" style="background:#f9f9f9">
+            <div class="container">
+                <div class="row gy-4">
+                    <div class="col-lg-5 col-md-12 footer-info">
+                        <a href="index.html" class="logo d-flex align-items-center">
+                            <img src="assets/img/logo-rd.svg" alt="footer" style="    height: 37px; " />
+                        </a>
+                        <p> At Comium work in favor of making communications ever convenient for our subscribers </p>
+                        <div class="social-links mt-3">
+                            <a href="https://twitter.com/ComiumGm" target="_blank" class="twitter">
+                                <i class="bi bi-twitter-x"></i>
+                            </a>
+                            <a href="https://www.facebook.com/Comium.Gambia" target="_blank" class="facebook">
+                                <i class="bi bi-facebook"></i>
+                            </a>
+                            <a href="https://www.instagram.com/comium.gm/" target="_blank" class="instagram">
+                                <i class="bi bi-instagram"></i>
+                            </a>
+                            <a href="https://www.linkedin.com/company/comium-gambia/" target="_blank" class="linkedin">
+                                <i class="bi bi-linkedin"></i>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-12 footer-contact  text-md-start useful-link">
+                        <h4>Useful Links</h4>
+                        <ul class="no-bullets">
+                            <li>
+                                <i class="bi bi-chevron-right"></i>
+                                <a href="#products">Products & Services</a>
+                            </li>
+                            <li>
+                                <i class="bi bi-chevron-right"></i>
+                                <a href="#about">About</a>
+                            </li>
+                            <li>
+                                <i class="bi bi-chevron-right"></i>
+                                <a href="#careers">Careers</a>
+                            </li>
+                            <li>
+                                <i class="bi bi-chevron-right"></i>
+                                <a href="#contact">Contact</a>
+                            </li>
+                            <li>
+                                <i class="bi bi-chevron-right"></i>
+                                <a href="/press-release">Press Release</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-3 col-md-12 footer-contact  text-md-start">
+                        <h4>Contact Us</h4>
+                        <p> 27 Kairaba Avenue <br /> Pipeline,KSMD <br /> Banjul-The Gambia <br />
+                            <strong>Monday</strong> to <strong>Friday</strong> 8:30 AM - 5:00 PM</strong> <br />
+                            <strong>Saturday:</strong> 9:00 AM - 2:00 PM <br />
+                            <strong>Phone:</strong>
+                            <a href="tel:+220 6 601 111" style="color:black" onMouseOver="this.style.color='red'" onMouseOut="this.style.color='black'">+220 6 601 111</a>
+                            <br />
+                            <strong>Email:</strong>
+                            <a href="mailto:info@comium.gm " style="color:black" onMouseOver="this.style.color='red'" onMouseOut="this.style.color='black'">info@comium.gm</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="copyright"> &copy; Copyright <strong>
+                    <span>Comium</span>
+                </strong>. All Rights Reserved </div>
+            <div class="credits"></div>
+        </div>
+    </footer>
+    <!-- End Footer -->
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center">
+        <i class="bi bi-arrow-up-short"></i>
+    </a>
+
+
+    <!-- Vendor JS Files -->
+
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/main.js?v9"></script>
+    <script src="assets/js/jquery-3.5.0.min.js"></script>
+    <script src="assets/js/slick.min.js"></script>
+
+    <script src="assets/js/mixall.js?v10"></script>
+
+    <script src="assets/vendor/aos/aos.js"></script>
+    <script src="assets/international-roaming/select2.min.js"></script>
+    <script src="assets/international-roaming/script.js?v=1"></script>
+
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-XJHHRG8NCG"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+        gtag('config', 'G-XJHHRG8NCG');
+    </script>
+</body>
+
+</html>
